0b9aa852c842b416673c993b46aad9399cbf4a38
lancelot-tanpj <40459264+lancelot-tanpj@users.noreply.github.com>
2018-10-08 13:10:24 +0200

    Add files via upload

---
+++ task.py
@@ -0,0 +1,69 @@
+import numpy as np
+from physics_sim import PhysicsSim
+
+class Task():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., target_pos=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat =  1 #3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        #reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        
+        x_diff = abs(self.sim.pose[0]-self.target_pos[0])
+        y_diff = abs(self.sim.pose[1]-self.target_pos[1])
+        z_diff = abs(self.sim.pose[2]-self.target_pos[2])
+        dist = np.sqrt(x_diff*2+y_diff**2+z_diff**2)
+        
+        #euler_diff = self.sim.pose[3:] - self.target_pos[3:]
+        #euler_dist = np.sqrt(np.dot(euler_diff,euler_diff))
+        #e1_diff = abs(self.sim.pose[3]-self.target_pos[3])
+        #e2_diff = abs(self.sim.pose[4]-self.target_pos[4])
+        #e3_diff = abs(self.sim.pose[5]-self.target_pos[5])
+        
+        #reward = 1 if dist < 0.1 else -1
+        
+        #reward = 1 if z_diff < 0.1 else -1
+        
+        k = 10
+        r = 1
+        reward = k if dist < r else k*np.exp(k*(r-dist))
+        
+        return reward
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward() 
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
